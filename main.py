b0a73ce85f81c261b14dbfd14d819db8a6f57895
Chernyshov_Nikita <xpom5566@mail.ru>
2020-04-09 13:55:50 +0300

    chore: thinking comments about solving task =)

--- main.py
+++ main.py
@@ -1,4 +1,34 @@
+from typing import List
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        print('hello')
+
+
+
 my = Solution()
-n = 0
-ans = my.(n)
+n =  [1,2,3,4]
+ans = my.productExceptSelf(n)
 print("ans", ans)
+
+
+
+
+#          24
+#    2           12
+# 1     2     3     4
+
+
+# x * y * z * m * n
+
+
+# THIS SHOULD WORK =)
+# [1,2,3,4]
+#     up-down   down-up
+# 0 - 1         24
+# 1 - 2         24
+# 3 - 6         12
+# 4 - 24        4
+
+# x[i] = UD[i - 1] * DU[i + 1]
